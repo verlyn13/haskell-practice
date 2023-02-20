0b82746d702c8c7b92eda6330895169e88d7eab9
verlyn13 <jeffreyverlynjohnson@gmail.com>
2023-02-19 21:07:20 -0900

    Set expressions

--- functions/mathfun.hs
+++ functions/mathfun.hs
@@ -6,3 +6,4 @@ doubleSmallNumber x =
         else x*2
 doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
 conanO'Brien = "It's a-me, Conan O'Brien!" 
+boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
